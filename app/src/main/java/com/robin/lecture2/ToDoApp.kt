9ad7cc34166de8741845622f22ad6656a7a6da2a
YasinDerwish <129393934+YasinDerwish@users.noreply.github.com>
2024-11-13 14:08:23 +0100

    NOTES APP
    Appen är nu klar, och det sista jag gjorde var att lägga till en funktion där man kan se vilken tid och datum som man senaste skapade/ändrat på sin Note.

--- app/src/main/java/com/robin/lecture2/ToDoApp.kt
+++ app/src/main/java/com/robin/lecture2/ToDoApp.kt
@@ -10,21 +10,23 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
-import androidx.compose.material3.SnackbarHostState
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 data class TodoItem(
     val id: Int,
     var title: String,
     var subtitle: String,
+    val timestamp:String,
     val check: MutableState<Boolean> = mutableStateOf(false)
 )
 
@@ -67,8 +69,22 @@ fun TodoListScreen(navController: NavController, todoList: MutableList<TodoItem>
                             onCheckedChange = {
                                 item.check.value = !item.check.value
                         })},
-                    headlineContent = { Text(item.title) },
-                    supportingContent = { Text(item.subtitle)},
+                    headlineContent = {Text(item.title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis) },
+                    supportingContent = {
+                        Text(
+                            item.subtitle,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = "Added on: ${item.timestamp}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+            },
+
                     trailingContent = {
                         Row {
                             IconButton(
@@ -96,6 +112,9 @@ fun TodoListScreen(navController: NavController, todoList: MutableList<TodoItem>
 fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>) {
     var title by remember { mutableStateOf("") }
     var subtitle by remember { mutableStateOf("") }
+    var isSaving by remember { mutableStateOf(false) }
+
+    val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
 
     val isFormValid = title.length >= 3 && subtitle.length >= 3
 
@@ -111,10 +130,14 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
                                     TodoItem(
                                         id = todoList.size,
                                         title = title,
-                                        subtitle = subtitle
+                                        subtitle = subtitle,
+                                        timestamp = timestamp
                                     )
                                 )
                                 navController.popBackStack()
+                            }
+                            else{
+                                isSaving = true
                             }
                         }
                     ) {
@@ -135,14 +158,14 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
                 value = title,
                 onValueChange = { title = it },
                 label = { Text("Todo") },
-                isError = title.length < 3
+                isError = isSaving && title.length < 3
             )
             Spacer(modifier = Modifier.height(16.dp))
             TextField(
                 value = subtitle,
                 onValueChange = { subtitle = it },
                 label = { Text("Details") },
-                isError = subtitle.length < 3
+                isError = isSaving && subtitle.length < 3
             )
             Spacer(modifier = Modifier.height(16.dp))
             Button(
@@ -152,7 +175,8 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
                             TodoItem(
                                 id = todoList.size,
                                 title = title,
-                                subtitle = subtitle
+                                subtitle = subtitle,
+                                timestamp = timestamp
                             )
                         )
                         navController.popBackStack()
@@ -163,7 +187,7 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
             {
                 Text("Add Todo")
             }
-            if (!isFormValid) {
+            if (isSaving && !isFormValid) {
                 Text(
                     text = "Title and/or Subtitle must be at least 3 characters.",
                     color = MaterialTheme.colorScheme.error,
@@ -180,8 +204,11 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
     fun EditTodoScreen(navController: NavController, todoItem: TodoItem) {
         var title by remember { mutableStateOf(todoItem.title) }
         var subtitle by remember { mutableStateOf(todoItem.subtitle) }
+        var isSaving by remember { mutableStateOf(false) }
         val snackbarHostState = remember { SnackbarHostState() }
         val coroutineScope = rememberCoroutineScope()
+        val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+
 
         Scaffold(
             topBar = {
@@ -206,13 +233,15 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
                 TextField(
                     value = title,
                     onValueChange = { title = it },
-                    label = { Text("Todo") }
+                    label = { Text("Todo") },
+                    isError = isSaving && title.length <3
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 TextField(
                     value = subtitle,
                     onValueChange = { subtitle = it },
-                    label = { Text("Details") }
+                    label = { Text("Details") },
+                    isError = isSaving && subtitle.length < 3
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
@@ -225,8 +254,19 @@ fun AddTodoScreen(navController: NavController, todoList: MutableList<TodoItem>)
 
                         }
                     }
+                    else{
+                        isSaving = true
+                    }
                 }) {
                     Text("Save ")
+                }
+                if (isSaving && (title.length <3 || subtitle.length < 3)){
+                    Text(
+                        text = "Title and/or Subtitle must be at least 3 characters.",
+                        color = MaterialTheme.colorScheme.error,
+                        style  = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
                 }
             }
         }
